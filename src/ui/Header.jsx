2dcc58b520284698122e4d73c24f7e0f1253c7ca
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-01 02:26:57 +0900

    Working With Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,13 +3,13 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
 	return (
-		<header>
-			<Link to="/">Fast React Pizza Co.</Link>
-			<SearchOrder />
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
 
-			<p>Jonas</p>
-		</header>
-	);
+      <p>Jonas</p>
+    </header>
+  );
 }
 
 export default Header;
