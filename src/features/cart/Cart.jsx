38f26216825fac46ede8f85f436c6a9007250bbf
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-01 23:40:20 +0900

    Styling Buttons:Element States and Transitions

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -29,17 +29,22 @@ function Cart() {
 	const cart = fakeCart;
 
 	return (
-		<div>
-			<Link to="/menu">&larr; Back to menu</Link>
+    <div>
+      <Link
+        to="/menu"
+        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+      >
+        &larr; Back to menu
+      </Link>
 
-			<h2>Your cart, %NAME%</h2>
+      <h2>Your cart, %NAME%</h2>
 
-			<div>
-				<Link to="/order/new">Order pizzas</Link>
-				<button>Clear cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <Link to="/order/new">Order pizzas</Link>
+        <button>Clear cart</button>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
